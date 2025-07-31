8cc72947d2abda2277fb4106e6bd7f4dfb8d72a2
Kerem Celikman <kerem.celikman@surpay.com.tr>
2025-07-31 11:34:40 +0300

    program.cs düzeltildi

--- ApiProjeKampi.WebUı/Program.cs
+++ ApiProjeKampi.WebUı/Program.cs
@@ -1,17 +1,21 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// 1. Cookie authentication'ı ekle
+builder.Services.AddAuthentication("Cookies")
+    .AddCookie("Cookies", options =>
+    {
+        options.LoginPath = "/Login/Index"; // Giriş sayfası
+        options.AccessDeniedPath = "/Login/AccessDenied"; // Yetki reddi sayfası (isteğe bağlı)
+    });
 
 builder.Services.AddHttpClient();
-
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -20,6 +24,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+// 2. Authentication ve Authorization middleware'lerini sırayla ekle
+app.UseAuthentication(); // <- Bunu mutlaka Authorization'dan önce koy
 app.UseAuthorization();
 
 app.MapControllerRoute(
